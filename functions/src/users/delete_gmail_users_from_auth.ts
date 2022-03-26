b467611c231a0c2dfcb9bff4fc8db871b685cbd4
Kenny Miu <34613996+kmiu2@users.noreply.github.com>
2022-03-26 17:35:39 -0400

    feat: added delete auth users function (#2)
    * feat: added delete auth emails
    
    * fix: move code
    
    * fix: going to second page of users

--- functions/src/users/delete_gmail_users_from_auth.ts
+++ functions/src/users/delete_gmail_users_from_auth.ts
@@ -1,16 +1,40 @@
+import * as admin from 'firebase-admin'
+import { ALLOWED_EMAILS } from '../data/consts'
 import { https } from 'firebase-functions'
-import { credential, initializeApp } from 'firebase-admin'
 
-const firebaseApp = initializeApp({
-  credential: credential.applicationDefault(),
-  databaseURL: 'https://staugustinechsapp.firebaseio.com',
-})
+// const db = firestore(firebaseApp)
+// const users = await db.collection('users').get()
 
-export const deleteGmailUsersFromAuth = https.onRequest(async (req, res) => {
+export const deleteNonValidEmailDomains = https.onRequest(async (req, res) => {
   try {
+    const firebaseApp = admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      databaseURL: 'https://staugustinechsapp.firebaseio.com',
+    })
+    const auth = firebaseApp.auth()
+    let nextPageToken
+    let users = await auth.listUsers(1000, nextPageToken)
+    const results = []
+
+    // We have under 2000 users
+    for (let i = 0; i < 2; i++) {
+      for (const user of users.users) {
+        if (
+          !user.email!.endsWith('@ycdsbk12.ca') &&
+          !user.email!.endsWith('@ycdsb.ca') &&
+          !ALLOWED_EMAILS.includes(user.email!)
+        ) {
+          await auth.deleteUser(user.uid)
+          results.push(user.email)
+        }
+      }
+      nextPageToken = users.pageToken
+      users = await auth.listUsers(1000, nextPageToken)
+    }
+
     res.json({
       data: {
-        message: firebaseApp.name,
+        results,
       },
     })
   } catch (error) {
