28af59008ca31fe8bb9c5fd686479112b20d1a25
kmiu2 <kennymiu@rogers.com>
2022-03-26 01:13:49 -0400

    feat: added delete auth emails

--- functions/src/data/consts.ts
+++ functions/src/data/consts.ts
@@ -1,3 +1,10 @@
 export const STA_ANNOUNCEMENT_SITE_URL = 'https://staugustinechs.ca/printable/'
 export const STA_DAY_NUMBER_SITE_URL =
   'https://staugustinechs.netfirms.com/stadayonetwo/'
+export const ALLOWED_EMAILS = [
+  'kennymiu2@gmail.com',
+  'sachstesterforapple@gmail.com',
+  'stauchsappdev@gmail.com',
+  'cafelady@stachs.com',
+  'administration@stachs.com',
+]
--- functions/src/users/delete_gmail_users_from_auth.ts
+++ functions/src/users/delete_gmail_users_from_auth.ts
@@ -1,16 +1,36 @@
 import { https } from 'firebase-functions'
 import { credential, initializeApp } from 'firebase-admin'
+import { ALLOWED_EMAILS } from '../data/consts'
 
 const firebaseApp = initializeApp({
   credential: credential.applicationDefault(),
   databaseURL: 'https://staugustinechsapp.firebaseio.com',
 })
 
-export const deleteGmailUsersFromAuth = https.onRequest(async (req, res) => {
+// const db = firestore(firebaseApp)
+// const users = await db.collection('users').get()
+
+export const deleteNonValidEmailDomains = https.onRequest(async (req, res) => {
   try {
+    const auth = firebaseApp.auth()
+    const users = await auth.listUsers()
+
+    const results = []
+
+    for (const user of users.users) {
+      if (
+        !user.email!.endsWith('@ycdsbk12.ca') &&
+        !user.email!.endsWith('@ycdsb.ca') &&
+        !ALLOWED_EMAILS.includes(user.email!)
+      ) {
+        await auth.deleteUser(user.uid)
+        results.push(user.email)
+      }
+    }
+
     res.json({
       data: {
-        message: firebaseApp.name,
+        results,
       },
     })
   } catch (error) {
