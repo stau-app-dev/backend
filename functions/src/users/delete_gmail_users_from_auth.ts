f4b106951e3babc34cd4389b07fe01cfcdbe14c4
kmiu2 <kennymiu@rogers.com>
2022-03-26 02:12:11 -0400

    fix: going to second page of users

--- functions/src/users/delete_gmail_users_from_auth.ts
+++ functions/src/users/delete_gmail_users_from_auth.ts
@@ -1,31 +1,35 @@
-import { https } from 'firebase-functions'
-import { credential, initializeApp } from 'firebase-admin'
+import * as admin from 'firebase-admin'
 import { ALLOWED_EMAILS } from '../data/consts'
+import { https } from 'firebase-functions'
 
 // const db = firestore(firebaseApp)
 // const users = await db.collection('users').get()
 
 export const deleteNonValidEmailDomains = https.onRequest(async (req, res) => {
   try {
-    const firebaseApp = initializeApp({
-      credential: credential.applicationDefault(),
+    const firebaseApp = admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
       databaseURL: 'https://staugustinechsapp.firebaseio.com',
     })
-
     const auth = firebaseApp.auth()
-    const users = await auth.listUsers()
-
+    let nextPageToken
+    let users = await auth.listUsers(1000, nextPageToken)
     const results = []
 
-    for (const user of users.users) {
-      if (
-        !user.email!.endsWith('@ycdsbk12.ca') &&
-        !user.email!.endsWith('@ycdsb.ca') &&
-        !ALLOWED_EMAILS.includes(user.email!)
-      ) {
-        await auth.deleteUser(user.uid)
-        results.push(user.email)
+    // We have under 2000 users
+    for (let i = 0; i < 2; i++) {
+      for (const user of users.users) {
+        if (
+          !user.email!.endsWith('@ycdsbk12.ca') &&
+          !user.email!.endsWith('@ycdsb.ca') &&
+          !ALLOWED_EMAILS.includes(user.email!)
+        ) {
+          await auth.deleteUser(user.uid)
+          results.push(user.email)
+        }
       }
+      nextPageToken = users.pageToken
+      users = await auth.listUsers(1000, nextPageToken)
     }
 
     res.json({
