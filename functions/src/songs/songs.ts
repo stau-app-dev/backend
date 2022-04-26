9ba2d13c288f103c39ce2d4d2b2eff964aa627f0
kmiu2 <kennymiu@rogers.com>
2022-04-25 22:28:41 -0400

    fix: remove console logs

--- functions/src/songs/songs.ts
+++ functions/src/songs/songs.ts
@@ -44,13 +44,10 @@ export const getSongs = https.onRequest(async (req, res) => {
 export const addSong = https.onRequest(async (req, res) => {
   try {
     const { artist, name, creatorEmail } = JSON.parse(req.body)
-    console.log('creatorEmail: ', creatorEmail)
     if (!artist || !name || !creatorEmail) {
       res.status(400).send({ error: 'Invalid parameters' })
       return
     }
-
-    console.log('i get here')
 
     const userDoc = await db
       .collection(NEW_USERS_COLLECTION)
@@ -63,12 +60,7 @@ export const addSong = https.onRequest(async (req, res) => {
     }
     const userId = userDoc.docs[0].id
     const user = userDoc.docs[0].data() as User
-    console.log('do u die at date')
     const lastSubmittedSong = user.lastSubmittedSong as any
-
-    console.log('i get here 2')
-    console.log('lastSubmittedSong: ', lastSubmittedSong)
-    console.log(typeof lastSubmittedSong)
 
     if (Date.now() - 24 * 60 * 60 * 1000 < lastSubmittedSong.toDate()) {
       res
@@ -76,8 +68,6 @@ export const addSong = https.onRequest(async (req, res) => {
         .send({ error: 'You can only submit a song once every 24 hours' })
       return
     }
-
-    console.log('i get here 3')
 
     const song = {
       artist,
@@ -102,7 +92,6 @@ export const addSong = https.onRequest(async (req, res) => {
     })
   } catch (error) {
     if (error instanceof Error) {
-      console.log('error: ', error.message)
       res.status(500).json({
         error: {
           message: error.message,
