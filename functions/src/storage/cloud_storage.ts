da93b68e97707a68086a6fd9cd95f64232609091
kmiu2 <kennymiu@rogers.com>
2022-04-28 12:14:26 -0400

    fix: getCafeMenuPictures storage pics

--- functions/src/storage/cloud_storage.ts
+++ functions/src/storage/cloud_storage.ts
@@ -10,3 +10,27 @@ export const getSignedUrlFromFilePath = async (filePath: string) => {
     })
   return fileUrl[0]
 }
+
+export const getFileNamesFromFolder = async (
+  folderPath: string,
+  limit?: number
+) => {
+  // Need to add one to the limit to account for the folder itself
+  const limitParam = limit ? limit + 1 : undefined
+
+  let fileNames = await storage
+    .bucket()
+    .getFiles({
+      prefix: folderPath,
+      maxResults: limitParam,
+    })
+    .then((files) => {
+      return files[0].map((file) => file.name)
+    })
+
+  // Remove the first index because it is the folder path
+  fileNames = fileNames.slice(1)
+
+  // Remove folder path from the file names
+  return fileNames.map((fileName) => fileName.replace(`${folderPath}/`, ''))
+}
