5f984091117e3d8dec391e64b4be6865c196e198
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-29 18:37:09 -0400

    feat: add status check to auth middleware

--- functions/src/middleware/auth.ts
+++ functions/src/middleware/auth.ts
@@ -1,8 +1,15 @@
-import { admin } from '../admin'
+import { admin, db } from '../admin'
 import { Response } from 'firebase-functions'
 import { Request } from '../types'
+import { GENERIC_ERROR_MESSAGE, NEW_USERS_COLLECTION } from '../data/consts'
+import { User } from '../models/users'
 
-const authMiddleware = async (req: Request, res: Response, next: Function) => {
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: Function,
+  requiredStatus: Number = 0
+) => {
   if (
     !req.headers.authorization ||
     !req.headers.authorization.startsWith('Bearer ')
@@ -13,10 +20,41 @@ const authMiddleware = async (req: Request, res: Response, next: Function) => {
   const idToken = req.headers.authorization.split('Bearer ')[1]
   try {
     const decodedIdToken = await admin.auth().verifyIdToken(idToken)
+    const userDoc = await db
+      .collection(NEW_USERS_COLLECTION)
+      .where('email', '==', decodedIdToken.email)
+      .get()
+    if (userDoc.empty) {
+      res.status(403).send({ error: 'Unauthorized' })
+      return
+    }
+    if ((userDoc.docs[0].data() as User).status < requiredStatus) {
+      res.status(403).send({ error: 'Unauthorized' })
+      return
+    }
+
     req.user = decodedIdToken
     next()
-  } catch (error) {
-    res.status(403).send({ error: 'Unauthorized' })
+  } catch (error: any) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        error: {
+          message: error.message,
+        },
+      })
+    } else if (error.code && error.code === 'auth/id-token-expired') {
+      res.status(401).json({
+        error: {
+          message: 'Token expired',
+        },
+      })
+    } else {
+      res.status(500).json({
+        error: {
+          message: GENERIC_ERROR_MESSAGE,
+        },
+      })
+    }
   }
 }
 
