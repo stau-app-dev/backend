855a9fde66ff36f4d1298804b08112c458c39dfc
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-29 22:53:54 -0400

    fix: cors on general topic notif

--- functions/src/notifications/topic.ts
+++ functions/src/notifications/topic.ts
@@ -1,47 +1,49 @@
 import { https } from 'firebase-functions'
-import { admin } from '../admin'
+import { admin, cors } from '../admin'
 import { GENERAL_TOPIC, GENERIC_ERROR_MESSAGE } from '../data/consts'
 
 export const sendToGeneralTopic = https.onRequest(async (req, res) => {
-  try {
-    const { message } = JSON.parse(req.body)
+  cors(req, res, async () => {
+    try {
+      const { message } = JSON.parse(req.body)
 
-    if (typeof message !== 'string') {
-      res.status(400).json({
-        error: {
-          message: 'Message is required',
+      if (typeof message !== 'string') {
+        res.status(400).json({
+          error: {
+            message: 'Message is required',
+          },
+        })
+        return
+      }
+
+      const data = {
+        notification: {
+          title: 'New Announcement from St. Augustine CHS',
+          body: message,
+        },
+      }
+
+      const response = await admin.messaging().sendToTopic(GENERAL_TOPIC, data)
+
+      res.json({
+        data: {
+          response,
         },
       })
-      return
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({
+          error: {
+            message: error.message,
+          },
+        })
+      } else {
+        res.status(500).json({
+          error: {
+            message: GENERIC_ERROR_MESSAGE,
+          },
+        })
+      }
     }
-
-    const data = {
-      notification: {
-        title: 'New Announcement from St. Augustine CHS',
-        body: message,
-      },
-    }
-
-    const response = await admin.messaging().sendToTopic(GENERAL_TOPIC, data)
-
-    res.json({
-      data: {
-        response,
-      },
-    })
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({
-        error: {
-          message: error.message,
-        },
-      })
-    } else {
-      res.status(500).json({
-        error: {
-          message: GENERIC_ERROR_MESSAGE,
-        },
-      })
-    }
-  }
+  })
 })
