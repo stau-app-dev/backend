f27c5e25e86a37144300eece390ba91640b523db
kmiu2 <kennymiu@rogers.com>
2022-05-09 20:10:31 -0400

    fix: bucket name

--- functions/src/db/backup.ts
+++ functions/src/db/backup.ts
@@ -3,13 +3,14 @@ import { client } from '../admin'
 
 export const backupFirestore = pubsub.schedule('0 0 * * *').onRun(async () => {
   try {
-    const bucket = 'gs://staugustinechsapp.appspot.com'
+    console.log('Beginning backup...')
+    const bucketName = 'gs://firestore-backup-bucket'
     const projectId = 'staugustinechsapp'
     const databaseName = client.databasePath(projectId, '(default)')
 
     const responses = await client.exportDocuments({
       name: databaseName,
-      outputUriPrefix: bucket,
+      outputUriPrefix: bucketName,
     })
 
     for await (const response of responses) {
