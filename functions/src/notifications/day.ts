0e69724b3e3ee4b5042fa235917a85b3c09278b7
Kaden司徒逸希 <kaden.seto06@gmail.com>
2023-11-23 14:53:43 -0500

    feat: notification test

---
+++ functions/src/notifications/day.ts
@@ -0,0 +1,44 @@
+import * as functions from 'firebase-functions';
+
+import { logger } from 'firebase-functions'
+import { GENERAL_TOPIC, STA_DAY_NUMBER_SITE_URL } from '../data/consts'
+import { DayNumber } from '../models/home'
+import { get } from 'request-promise'
+import { backOff } from 'exponential-backoff'
+
+import * as admin from 'firebase-admin';
+
+export const dayNumberNotification = functions.pubsub.schedule('every day 08:30').timeZone('UTC-5').onRun((context) => {  
+    const searchString = 'Day ';
+    let data: String;
+
+    try {
+      data = backOff(async () => get(STA_DAY_NUMBER_SITE_URL)).toString();
+      logger.log('Fetched day number data: '+data);
+    } catch (error) {
+      logger.error('Error getting day number data: ' + error)
+      return
+    }
+
+    const dayNumber: DayNumber['dayNumber'] = parseInt(
+      data.substring(
+        data.lastIndexOf(searchString) + searchString.length,
+        data.lastIndexOf(searchString) + searchString.length + 1
+      )
+    )
+
+    const notif = {
+      notification: {
+        title: 'Good morning, Titans.',
+        body: `Today is Day ${dayNumber}!`,
+      },
+    }
+
+    try {
+      admin.messaging().sendToTopic(GENERAL_TOPIC, notif)
+      logger.log('Sent day number notifications to everyone')
+    } catch (error) {
+      logger.error('Error sending Day Number Notifications: ' + error)
+    }
+  });
+
