8f4bcff51587fa67555dab43b833cd146139170d
Alfred Cadawas <alfred.cadawas@gmail.com>
2026-01-09 09:13:58 -0500

    depreciating old general annoucements function

--- functions/src/announcements/general_announcements.ts
+++ functions/src/announcements/general_announcements.ts
@@ -1,10 +1,6 @@
 import { https } from 'firebase-functions'
-import { get } from 'request-promise'
 import * as cors from 'cors'
-import {
-  GENERIC_ERROR_MESSAGE,
-  STA_ANNOUNCEMENT_SITE_URL,
-} from '../data/consts'
+import { GENERIC_ERROR_MESSAGE } from '../data/consts'
 import { GeneralAnnouncement } from '../models/announcements'
 import { db, admin } from '../admin'
 
@@ -30,62 +26,18 @@ const corsHandler = cors({
 })
 
 export const getGeneralAnnouncements = https.onRequest((req, res) => {
-  corsHandler(req, res, async () => {
-    const startString = 'ancmnt = "'
-    const endString = '".split(",");'
-    const splitString = '$%-%$'
+  corsHandler(req, res, () => {
+    const formatted: GeneralAnnouncement[] = [
+      {
+        title: 'New App',
+        content:
+          'This app version is no longer supported. Please use the latest version at https://staugustinecha.ca',
+      },
+    ]
 
-    try {
-      const data: string = await get({
-        uri: STA_ANNOUNCEMENT_SITE_URL,
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0',
-        },
-      })
-
-      const rawHTML = data.substring(
-        data.indexOf(startString) + startString.length,
-        data.indexOf(endString)
-      )
-
-      const announcements = rawHTML.split(',')
-
-      if (
-        announcements.length === 0 ||
-        (announcements.length === 1 &&
-          announcements[0].toLowerCase().trim() === 'no announcements today')
-      ) {
-        res.set('Access-Control-Allow-Origin', req.headers.origin || '')
-        res.set('Vary', 'Origin')
-        res.json({ data: [] })
-        return
-      }
-
-      const formatted: GeneralAnnouncement[] = announcements.map(
-        (htmlAnnouncement) => {
-          const announcement = decodeURIComponent(htmlAnnouncement)
-          const [title, content] = announcement.split(splitString)
-          return {
-            title: title.trim(),
-            content: content.trim(),
-          }
-        }
-      )
-
-      res.set('Access-Control-Allow-Origin', req.headers.origin || '')
-      res.set('Vary', 'Origin')
-      res.json({ data: formatted })
-    } catch (error) {
-      res.set('Access-Control-Allow-Origin', req.headers.origin || '')
-      res.set('Vary', 'Origin')
-      res.status(500).json({
-        error: {
-          message:
-            error instanceof Error ? error.message : GENERIC_ERROR_MESSAGE,
-        },
-      })
-    }
+    res.set('Access-Control-Allow-Origin', req.headers.origin || '')
+    res.set('Vary', 'Origin')
+    res.json({ data: formatted })
   })
 })
 
